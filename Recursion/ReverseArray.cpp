1fc7529b1fce34bfc0b51e339bb813463b2b3dee
surajnimbalkar07 <136218136+surajnimbalkar07@users.noreply.github.com>
2024-10-11 20:02:26 +0530

    reverse array

---
+++ Recursion/ReverseArray.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void reverseArray(vector<int> &arr) {
+        
+        int n=arr.size()-1;
+        if (arr.size() <= 1) return; 
+        
+        swap(arr[0],arr[n]);
+        
+        vector<int> subArray(arr.begin() + 1, arr.end() - 1);
+        
+        reverseArray(subArray);
+        
+        for(int i=i;i<=arr.size()-1;++i){
+            arr[i]=subArray[i-1];
+        }
+}
+
+int main(){
+    vector<int> arr = {1, 2, 3, 4, 5};
+    reverseArray(arr);
+    cout << "Reversed array: ";
+    for (int i : arr) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
